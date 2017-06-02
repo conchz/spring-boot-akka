a5461e53ae07807cb5f4d9313063697807493b09
Baymax <dolphineor@gmail.com>
2017-06-02 18:00:55 +0800

    Update

---
+++ common/src/main/scala/demo/spring/akka/SpringActor.scala
@@ -0,0 +1,13 @@
+package demo.spring.akka
+
+import akka.actor.{Actor, ActorContext}
+
+/**
+ * Extend this trait to add factory helper methods to actors.
+ */
+trait SpringActor extends Actor with SpringActorRefFactory {
+  this: Actor =>
+
+  protected implicit val factory: ActorContext = context
+
+}
